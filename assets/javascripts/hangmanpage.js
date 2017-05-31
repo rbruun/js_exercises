7cbc15561628c7f13d28a7a2350bed1d571c5f69
Ron Bruun <ronald.bruun@libertymutual.com>
2017-05-31 17:03:23 -0400

    added call to web service for word

--- assets/javascripts/hangmanpage.js
+++ assets/javascripts/hangmanpage.js
@@ -4,17 +4,37 @@
 
     //make sure the page is loaded
     window.onload = function () {
-        
-        let word = "window"
+
+        function RandomWord() {
+            var requestStr = "http://setgetgo.com/randomword/get.php?len=6";
+            $.ajax({
+                type: "GET",
+                url: requestStr,
+                dataType: "jsonp",
+                success: function(data){
+                        setWordLetters(data.Word);
+            }
+        })};
+
+
+        RandomWord();
         let hangman = [];
         let wrong = 0;
-        let wordList = word.split("");
-        let pageWord = document.querySelector("#word");
-        //add letter placeholder divs on page equal to number of letters in word
-        for (let i = 0; i < word.length; i++) {
-            pageWord.innerHTML = pageWord.innerHTML + "<div class='letter'></div>";
+        let word = "default";
+        let wordList = [];
+        let pageWord;
+        let guesses = document.querySelector("#guesses");
+        
+        function setWordLetters(inword){
+console.log("the word is: " + inword);
+            word = inword;
+            wordList = word.split("");
+            pageWord = document.querySelector("#word");
+            //add letter placeholder divs on page equal to number of letters in word
+            for (let i = 0; i < word.length; i++) {
+                pageWord.innerHTML = pageWord.innerHTML + "<div class='letter'></div>";
+            }
         }
-
         let message = document.querySelector("#message");
 
         // load all of the body parts in to an array
@@ -27,11 +47,12 @@
 
         document.querySelector("#submit").addEventListener("click", function () {
             let letterEntered = document.querySelector("#letterEntered").value.toLowerCase();
+            
+            addGuesses(letterEntered);
 
             // check if the entered letter is in the word
             if (letterEntered == "") {
                 message.innerHTML = "Please enter a letter";
-                
             } else if (word.indexOf(letterEntered) < 0) {
                 hangman[wrong].removeAttribute("class");
                 wrong += 1;
@@ -40,20 +61,23 @@
                     message.innerHTML = message.innerHTML + ",  you Lose!!";
                     // show the dead eyes
                     document.querySelector("#deadeye").removeAttribute("class");
-                    
+
                     // hide the input box and submit button
                     document.querySelector("#inputLetter").className = "hide";
-                    
+
                     // show the start over button
                     document.querySelector("#reset").removeAttribute("class");
+                    
+                    // populate the screen with the word
+                    setLoserWord();
                 }
             } else {
                 message.innerHTML = "Good guess!!";
                 updateWord(letterEntered);
             }
         });
-        
-        document.querySelector("#reset").addEventListener("click", function(){
+
+        document.querySelector("#reset").addEventListener("click", function () {
             window.location.reload();
         });
 
@@ -65,7 +89,18 @@
                 }
             }
         }
+        
+        function addGuesses(letter) {
+            guesses.innerHTML = guesses.innerHTML + "<div class='letter'>" + letter + "</div>";
+        }
+        
+        function setLoserWord() {
+            for (let i = 0; i < wordList.length; i++) {
+                pageWord.childNodes[i + 1].innerHTML = wordList[i];
+            }
+        }
+
     }
-    
+
 
 })();
